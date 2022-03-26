bc0d96c6c908baf4da0ff6ce4a1f286da357fce6
yuniskhamis <yuniskhamis3@gmail.com>
2022-03-26 01:25:51 -0400

    Register post class

--- app/src/main/java/com/yunis/parstagram/ParstagramApplication.kt
+++ app/src/main/java/com/yunis/parstagram/ParstagramApplication.kt
@@ -3,11 +3,14 @@ package com.yunis.parstagram
 import com.parse.Parse
 
 import android.app.Application
+import com.parse.ParseObject
 
 
 class ParstagramApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        ParseObject.registerSubclass(Post::class.java)
+
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
